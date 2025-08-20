c9ac1edfbc9e766193ce5f530026d393ee7397c0
Narayana.Chatta <narayana.chatta@callenlenz.com>
2025-08-20 15:54:38 +0100

    18

--- Test_Cases/Test_Procedures/SCF_Open.py
+++ Test_Cases/Test_Procedures/SCF_Open.py
@@ -114,7 +114,8 @@ if len(good_matches) > 10:
         out.write('| Title: UpdateRate_Window.png |\n')
         out.write('| :---------------------------- |\n')
         out.write('| ![Test Image](../Test_Images/UpdateRate_Window.png) |\n')
-        out.write(" *Figure1: Test Image for comparing results* |\n")
+        out.write("| *Figure1: Test Image for comparing results* |\n")
+        out.write('----------------------------\n')
         
         
         out.write('![Result Image captured](../Result_Images/matched_region_highlighted.png)\n\n')
